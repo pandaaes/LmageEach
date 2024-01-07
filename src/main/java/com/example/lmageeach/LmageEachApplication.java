cd9e7de0eb1f624d427f99d4f06400513a707bb1
Liujh <3362564235@qq.com>
2024-01-07 15:16:06 +0800

    跨域

--- src/main/java/com/example/lmageeach/LmageEachApplication.java
+++ src/main/java/com/example/lmageeach/LmageEachApplication.java
@@ -4,11 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @MapperScan("com/example/lmageeach/mapper")
-@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(origins = "*")
 public class LmageEachApplication {
 
     public static void main(String[] args) {
